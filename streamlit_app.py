36f3fa5dd41acc4e0077d482e8430c725dc81d84
bruhwarda <wbywardah@gmail.com>
2023-12-19 12:23:47 +0500

    latest code

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with st.sidebar:
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
     
 # Store LLM generated responses
-if "messages" not in st.session_state.keys():
+if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
 # Display chat messages
@@ -32,12 +32,17 @@ for message in st.session_state.messages:
 
 # Function for generating LLM response
 def generate_response(prompt_input, email, passwd):
-    # Hugging Face Login
-    sign = Login(email, passwd)
-    cookies = sign.login()
-    # Create ChatBot                        
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    return chatbot.chat(prompt_input)
+    try:
+        # Hugging Face Login
+        sign = Login(email, passwd)
+        cookies = sign.login()
+
+        # Create ChatBot
+        chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+        response = chatbot.chat(prompt_input)
+        return response
+    except Exception as e:
+        return f"Error: {e}"
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
@@ -47,9 +52,8 @@ if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_response(prompt, hf_email, hf_pass) 
-            st.write(response) 
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+    with st.spinner("Thinking..."):
+        response = generate_response(prompt, hf_email, hf_pass)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            st.write(response)
